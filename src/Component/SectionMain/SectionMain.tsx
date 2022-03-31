9a1ef47bde985d838d2a39ab87c6b527d2a517b9
Semko1010 <semir01020@gmail.com>
2022-03-31 03:22:56 +0200

    v1.1

--- src/Component/SectionMain/SectionMain.tsx
+++ src/Component/SectionMain/SectionMain.tsx
@@ -1,10 +1,17 @@
 import "./SectionMain.css"
 import { Parallax } from 'react-parallax';
-
+import {useState} from 'react';
 
 const SectionMain = () =>{
+const [showDiv, setShowDiv] = useState<boolean>(false)
+const [showDiv2, setShowDiv2] = useState<boolean>(false)
 
-
+const showDivLeistung = ()=>{
+    setShowDiv(!showDiv)
+}
+const showDivLeistung2 = ()=>{
+    setShowDiv2(!showDiv2)
+}
     return(
         <section className="sectionMain">
             
@@ -25,12 +32,101 @@ const SectionMain = () =>{
     </Parallax>
              <Parallax className="parallax2" bgImage="/img/img2.jpg"  strength={800}>
              <div className="divSecondImg">
-                <h2>Ihr Wunsch ist Leidenschaft</h2>
-
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo natus quibusdam optio sit velit sint? Repudiandae fugit rem dolor iure sapiente dolorem illo eum, </p>
+                <h2>Unsere leistungen</h2>
+                <div className="leistungen"
+               
+                >
+                    <div>
+                        <h3 onClick={showDivLeistung}>Raumausstatter</h3>
+                        <div className="leistungOne"
+                         style={{
+                            height: showDiv ? "200px" : "0px",
+                            opacity: showDiv ? "1" : "0",
+                            backgroundColor: showDiv ? "gray": "white"
+                        }}
+                        >
+                            <p
+                             style={{
+                                opacity: showDiv ? "1" : "0",
+                                
+                            }}
+                            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, adipisci.</p>
+                        </div>
+                    </div>
+                    <div>
+                    <h3
+                    onClick={showDivLeistung2}
+                    >Raumausstatter</h3>
+                        <div className="leistungOne"
+                         style={{
+                            height: showDiv2 ? "200px" : "0px",
+                            opacity: showDiv2 ? "1" : "0",
+                            backgroundColor: showDiv2 ? "gray": "white"
+                        }}
+                        >
+                            <p
+                             style={{
+                                opacity: showDiv2 ? "1" : "0",
+                                
+                            }}
+                            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, adipisci.</p>
+                        </div>
+                    </div>
+                    <div>
+                    <h3>Raumausstatter</h3>
+                    <div className="leistungOne"
+                         style={{
+                            height: showDiv2 ? "200px" : "0px",
+                            opacity: showDiv2 ? "1" : "0",
+                            backgroundColor: showDiv2 ? "gray": "white"
+                        }}
+                        >
+                            <p
+                             style={{
+                                opacity: showDiv2 ? "1" : "0",
+                                
+                            }}
+                            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, adipisci.</p>
+                        </div>
+                    </div>
+                    <div>
+                    <h3>Raumausstatter</h3>
+                    <div className="leistungOne"
+                         style={{
+                            height: showDiv2 ? "200px" : "0px",
+                            opacity: showDiv2 ? "1" : "0",
+                            backgroundColor: showDiv2 ? "gray": "white"
+                        }}
+                        >
+                            <p
+                            style={{
+                                opacity: showDiv2 ? "1" : "0",
+                                
+                            }}
+                            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, adipisci.</p>
+                        </div>
+                    </div>
+                    <div>
+                    <h3>Raumausstatter</h3>
+                    <div className="leistungOne"
+                         style={{
+                            height: showDiv2 ? "200px" : "0px",
+                            opacity: showDiv2 ? "1" : "0",
+                            backgroundColor: showDiv2 ? "gray": "white"
+                        }}
+                        >
+                            <p
+                             style={{
+                                opacity: showDiv2 ? "1" : "0",
+                                
+                            }}
+                            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, adipisci.</p>
+                        </div>
+                    </div>
+                </div>
                 <div>
-                <a href="https://the-movie-app-2021.netlify.app/" target="_blank" rel="noreferrer" className="movie-flip" data-back="Movie App" data-front="Mehr Erfahren"> </a>
-                <a href="https://the-movie-app-2021.netlify.app/" target="_blank" rel="noreferrer" className="movie-flip1" data-back="Movie App" data-front="Kontakt"> </a>
+                {/* <a href="https://the-movie-app-2021.netlify.app/" target="_blank" rel="noreferrer" className="movie-flip" data-back="Movie App" data-front="Mehr Erfahren"> </a>
+                <a href="https://the-movie-app-2021.netlify.app/" target="_blank" rel="noreferrer" className="movie-flip1" data-back="Movie App" data-front="Kontakt"> </a> */}
                 </div>
             </div>
     </Parallax>
